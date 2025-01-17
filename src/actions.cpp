085195f90cf838585d15a21b3478d6f690216362
longwatermelon <jomathyc@gmail.com>
2025-01-16 21:57:18 -0700

    update

--- src/actions.cpp
+++ src/actions.cpp
@@ -58,6 +58,29 @@ void bishop_moves(pt_t p, const state_t &s, vec<act_t> &moves) {
     }
 }
 
+void queen_moves(pt_t p, const state_t &s, vec<act_t> &moves) {
+    rook_moves(p,s,moves);
+    bishop_moves(p,s,moves);
+}
+
+void knight_moves(pt_t p, const state_t &s, vec<act_t> &moves) {
+    int d8i[]={-2,-1,1,2,2,1,-1,-2}, d8j[]={1,2,2,1,-1,-2,-2,-1};
+    for (int k=0; k<8; ++k) {
+        if (!MV(p, p+pt_t{d8i[k],d8j[k]}, s, moves)) {
+            CA(p, p+pt_t{d8i[k],d8j[k]}, s, moves);
+        }
+    }
+}
+
+void king_moves(pt_t p, const state_t &s, vec<act_t> &moves) {
+    int d8i[]={-1,-1,0,1,1,1,0,-1}, d8j[]={0,1,1,1,0,-1,-1,-1};
+    for (int k=0; k<8; ++k) {
+        if (!MV(p, p+pt_t{d8i[k],d8j[k]}, s, moves)) {
+            CA(p, p+pt_t{d8i[k],d8j[k]}, s, moves);
+        }
+    }
+}
+
 // actions for current state
 vec<act_t> actions(const state_t &s, bool legal) {
     vec<act_t> moves;
@@ -68,6 +91,9 @@ vec<act_t> actions(const state_t &s, bool legal) {
                 case 'p': pawn_moves({i,j}, s, moves); break;
                 case 'r': rook_moves({i,j}, s, moves); break;
                 case 'b': bishop_moves({i,j}, s, moves); break;
+                case 'q': queen_moves({i,j}, s, moves); break;
+                case 'n': knight_moves({i,j}, s, moves); break;
+                case 'k': king_moves({i,j}, s, moves); break;
                 }
             }
         }
